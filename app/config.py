207911ff0c556c1840e69c73e1b18bcb9b2e5836
Loic191 <loic.masureel@ugent.be>
2024-11-20 08:55:51 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
 class Config: 
+    SECRET_KEY = Spinnback123
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ydlbtcbabebtcajmvoyl:Spinnback123#@aws-0-eu-west-3.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
