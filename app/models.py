063fb073f819fd6e7a39de153abf9479b657a3d9
EliasDeBruyne <eliasdbr.debruyne@ugent.be>
2024-11-27 09:35:00 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
 class Users(db.Model):
     __tablename__ = 'users'
+    
     userid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)  # Username should be unique
     email = db.Column(db.String(80), unique=True, nullable=False)  # Email should also be unique
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Timestamp for when the user was created
+    address = db.Column(db.String(255))  # Address of the user
+
     libraries = db.relationship('Libraries', backref='user', lazy=True)
     buyers = db.relationship('Buyers', backref='user', lazy=True, uselist=False)  # Assuming a user can be a buyer once
     sellers = db.relationship('Sellers', backref='user', lazy=True, uselist=False)  # Assuming a user can be a seller once
@@ -21,16 +26,7 @@ class Libraries(db.Model):
     userid = db.Column(db.Integer, db.ForeignKey('users.userid'), nullable=False)
     library_records = db.relationship('LibraryRecords', backref='library', lazy=True)
 
-# Buyers model
-class Buyers(db.Model):
-    __tablename__ = 'buyers'
-    buyerid = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('users.userid'), nullable=False, unique=True)  # One-to-one with user
-    address = db.Column(db.String(80), nullable=False)  # Assume multiple people can have the same address
-    transactions = db.relationship('Transactions', backref='buyer', lazy=True)
 
-    def __repr__(self):
-        return f'Address {self.address}'
 
 # Records model
 class Records(db.Model):
@@ -45,20 +41,13 @@ class Records(db.Model):
     description = db.Column(db.String(100), nullable=True)
     price = db.Column(db.Float, nullable=False)
     ownerid = db.Column(db.Integer, db.ForeignKey('users.userid'), nullable=False)  # Foreign key to Users
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  
     library_records = db.relationship('LibraryRecords', backref='record', lazy=True)
     transactions = db.relationship('Transactions', backref='record', lazy=True)
 
     def __repr__(self):
         return f'<Record {self.albumname}, ${self.price}>'
 
-# Sellers model
-class Sellers(db.Model):
-    __tablename__ = 'sellers'
-    sellerid = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('users.userid'), nullable=False, unique=True)  # One-to-one with user
-
-    def __repr__(self):
-        return f'<Seller ID {self.sellerid}>'
 
 # LibraryRecords model (linking libraries and records)
 class LibraryRecords(db.Model):
