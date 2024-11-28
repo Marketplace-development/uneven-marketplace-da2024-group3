7c9938a6b9ad854462a862154a12cd94808f06e1
IdesGernaey <ides.gernaey@ugent.be>
2024-11-28 17:27:18 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from supabase import create_client, Client
 from .config import Config
 from .models import db
 
-# Supabase Configuration
-SUPABASE_URL = "https://ydlbtcbabebtcajmvoyl.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkbGJ0Y2JhYmVidGNham12b3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MDY3MjksImV4cCI6MjA0NjM4MjcyOX0.dh9-iGEp9McKCGicE8PJN0DjlppPtUpGUMV2NP60HkM"
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 migrate = Migrate()
 
