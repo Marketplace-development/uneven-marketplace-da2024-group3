d874c45d04859cc6e24b0ce1be42475812188e9d
IdesGernaey <ides.gernaey@ugent.be>
2024-11-28 14:08:54 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 class Config: 
-    SECRET_KEY = 'Spinnback123#'
+    SECRET_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkbGJ0Y2JhYmVidGNham12b3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MDY3MjksImV4cCI6MjA0NjM4MjcyOX0.dh9-iGEp9McKCGicE8PJN0DjlppPtUpGUMV2NP60HkM'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ydlbtcbabebtcajmvoyl:Spinnback123#@aws-0-eu-west-3.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
